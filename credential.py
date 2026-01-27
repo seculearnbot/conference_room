e1ba9e2b8df5000f4ed87e1c4eaa6766fabff432
seculearnbot <163666946+seculearnbot@users.noreply.github.com>
2026-01-28 03:29:31 +0330

    Update credential.py

--- credential.py
+++ credential.py
@@ -1,18 +1,14 @@
 
 import os
 
-
 secret_key = os.environ.get('SECRET_KEY', 'development-secret-key-123')
-
 student_file_path = "static/storage/student/"
 file_path = "static/storage/teacher/"
-
-
 GOOGLE_CLIENT_ID = "1050801722055-btcvqar75jhdt6shnop5esohpj5avd5c.apps.googleusercontent.com"
 
 
-host = os.environ.get('DB_HOST', 'localhost')
-port = int(os.environ.get('DB_PORT', 3306)) 
-user = os.environ.get('DB_USER', 'root')
-password = os.environ.get('DB_PASSWORD', '')
-databasename = os.environ.get('DB_NAME', 'conference_room')  
+host = "dpg-d5sl1bn5r7bs73b3il50-a.virginia-postgres.render.com" 
+port = 5432
+user = "daneshclass_db_user"
+password = "fCE7pGDPy1NlpbDsvRkcsiVkD7XEJaT3"
+databasename = "daneshclass_db"
